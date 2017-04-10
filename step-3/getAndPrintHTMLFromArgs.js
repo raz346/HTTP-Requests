24a22ef215a5f8c770383d48f0a61478ed76f1d5
Ahmed Al-Hashimi <Alhashimi346@gmail.com>
2017-04-10 19:58:27 +0000

    finish step-3 and submit js

---
+++ step-3/getAndPrintHTMLFromArgs.js
@@ -0,0 +1,26 @@
+
+
+const https = require('https');
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step1.html'
+};
+function getAndPrintHTMLChunks(option) {
+ 
+  let fullAddress = 'https://'+requestOptions.host + requestOptions.path;
+  // console.log(fullAddress);
+  https.get(fullAddress,(response) => {  
+     let chunkReceived = '';
+    // set encoding of received data to UTF-8
+    response.setEncoding('utf8');
+
+    // the callback is invoked when a `data` chunk is received
+    response.on('data',(data) => {
+      chunkReceived += data ;
+    });
+    response.on('end', () => {
+      console.log(chunkReceived);
+    });  
+  });
+}
+getAndPrintHTMLChunks(requestOptions);
